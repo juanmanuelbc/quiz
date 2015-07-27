73bb39f84651c36184743a84c2019356c843c3fb
juanmanuelbc <juanmanuelbc@gmail.com>
2015-07-27 23:46:41 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -30,7 +30,8 @@ sequelize.sync().then(function() {
   Quiz.count().then(function(count) {
     // La tabla se inicializa sólo si está vacía
     if (count === 0) {
-      Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma' }).then(function() {
+      Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma' });
+      Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa' }).then(function() {
 	console.log('Base de datos inicializada');
       });
     }
