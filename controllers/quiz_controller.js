9b5bafa89680490d8483f8d49043b673031fc678
juanmanuelbc <juanmanuelbc@gmail.com>
2015-07-30 00:21:14 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -63,3 +63,25 @@ exports.create = function (req, res) {
     }
   });
 };
+
+// GET /quizes/:quizId/edit
+exports.edit = function (req, res) {
+  res.render('quizes/edit', { quiz: req.quiz, errors: [] });
+};
+
+// PUT /quizes/:quizId
+exports.update = function (req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(function(err) {
+    if (err) {
+      res.render('quizes/edit', { quiz: req.quiz, errors: err.errors } );
+    }
+    else {
+      req.quiz.save({ fields: ['pregunta', 'respuesta'] }).then(function() {
+	res.redirect('/quizes');
+      });
+    }
+  });
+};
